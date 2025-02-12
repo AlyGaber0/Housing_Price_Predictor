f737e467cee282d97cb6d7ccc354226116e151c5
Aly Gaber <aly.gaber@mail.mcgill.ca>
2025-02-12 00:03:39 -0500

    kk

--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
 # Main file to write our code
 import pandas as pd
 
-pandas.read_csv('ottawa-realestate-data(ottawa-realestate-data).csv')
+first_ottawa_housing = pd.read_csv('ottawa-realestate-data(ottawa-realestate-data).csv')
+second_ottawa_housing = pd.read_csv('ca_real_estate.csv')
+
+#filter code to only show ottawa houses
+filtered_df = second_ottawa_housing[second_ottawa_housing\
+    ['City'].str.contains('Ottawa', case=False, na=False)]
+
+print(filtered_df)
