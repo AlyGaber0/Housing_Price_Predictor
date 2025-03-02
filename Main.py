cdc300dd3015bf278c5a654c5dd69f155ea8a005
Aly Gaber <aly.gaber@mail.mcgill.ca>
2025-03-02 00:01:41 -0500

    first

--- Main.py
+++ Main.py
@@ -8,5 +8,4 @@ second_ottawa_housing = pd.read_csv('ca_real_estate.csv')
 filtered_df = second_ottawa_housing[second_ottawa_housing\
     ['City'].str.contains('Ottawa', case=False, na=False)]
 
-print(filtered_df)
-ok
+filtered_df.to_csv('filtered_second.csv')
